004fcd94322f29db27241be761cd634a0208a745
FelipeFlister <153518854+FelipeFlister@users.noreply.github.com>
2025-07-20 18:26:57 -0300

    Adição de dropar lenha quando derrubar a árvore, e o jogador coleta

--- Scenes/Recourses/Trees/tree.gd
+++ Scenes/Recourses/Trees/tree.gd
@@ -1,12 +1,15 @@
 extends StaticBody2D
 class_name WorldTree
 
+const WOOD_COLLECTABLE: PackedScene = preload("res://Scenes/Collectables/wood.tscn")
 var is_dead: bool = false
 
 @export_category("Variables")
 @export var health: int
 @export var min_health: int = 10
 @export var max_health: int = 30
+@export var min_amount_of_spawned_wood: int = 1
+@export var max_amount_of_spawned_wood: int = 5
 
 @export_category("Objects")
 @export var animation: AnimationPlayer
@@ -26,6 +29,7 @@ func update_health(damage_range: Array) -> void:
 	if health <= 0:
 		is_dead = true
 		animation.play("kill")
+		spawn_wood()
 		return
 		
 	animation.play("hit")
@@ -33,4 +37,12 @@ func update_health(damage_range: Array) -> void:
 func _on_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "hit":
 		animation.play("idle")
-	pass # Replace with function body.
+func spawn_wood() -> void:
+	var amount_of_spawned_wood: int = randi_range(min_amount_of_spawned_wood, max_amount_of_spawned_wood) #Aleatoriza um número de lenhas para ser spawnado
+	for i in amount_of_spawned_wood: #Vai spawnar as lenhas de acordo com a quantidade e em posições aleatórias
+		var collectable_wood: CollectableComponent = WOOD_COLLECTABLE.instantiate()
+		collectable_wood.global_position = global_position + Vector2(
+			randi_range(-32, 32), randi_range(-32, 32)
+		)
+		
+		get_tree().root.call_deferred("add_child", collectable_wood)
---
+++ Scripts/Collectables/collectable_component.gd
@@ -0,0 +1,14 @@
+extends Area2D
+class_name CollectableComponent
+
+@export_category("Objects")
+@export var item_texture: Texture2D
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is BaseCharacter:
+		#body.add_item({
+				#"item_name": "lenha",
+				#"item_amount": [1, 5],
+				#"item_texture": item_texture
+		#})
+		queue_free() #Deleta o node do collectable
