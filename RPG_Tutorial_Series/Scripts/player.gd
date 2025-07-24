642854333641a7b0d69807b0a605b75eac61ff1b
FelipeFlister <153518854+FelipeFlister@users.noreply.github.com>
2025-07-24 08:38:56 -0300

    Adição de movimentação e animações

--- RPG_Tutorial_Series/Scripts/player.gd
+++ RPG_Tutorial_Series/Scripts/player.gd
@@ -1,8 +1,55 @@
 class_name Player extends CharacterBody2D
 
-const MOVE_SPEED: float = 100.0
+var cardinal_direction: Vector2 = Vector2.ZERO
+var direction: Vector2 = Vector2.ZERO
+var move_speed: float = 100.0
+var state: String = "idle"
 
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func  _process(delta: float) -> void:
+	direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * move_speed
+	
+	if set_state() or set_direction():
+		update_animation()
+	
 func _physics_process(delta: float) -> void:
-	var direction: Vector2 = Input.get_vector("left", "right", "up", "down")
-	velocity = direction * MOVE_SPEED
 	move_and_slide()
+
+func set_direction() -> bool:
+	var new_dir: Vector2 = cardinal_direction
+	if direction == Vector2.ZERO:
+		return false
+		
+	if direction.y == 0:
+		new_dir = Vector2.LEFT if direction.x < 0 else Vector2.RIGHT
+	elif direction.x == 0:
+		new_dir = Vector2.UP if direction.y < 0 else Vector2.DOWN
+		
+	if new_dir == cardinal_direction:
+		return false
+	
+	cardinal_direction = new_dir
+	sprite.scale.x = -1 if cardinal_direction == Vector2.LEFT else 1
+	return true	
+	
+func set_state() -> bool:
+	var new_state: String = "idle" if direction == Vector2.ZERO else "walk"
+	if new_state == state:
+		return false
+	state = new_state
+	return true
+	
+func update_animation() -> void:
+	animation_player.play(state + "_" + anim_direction())
+	pass
+	
+func anim_direction() -> String:
+	if cardinal_direction == Vector2.DOWN:
+		return "down"
+	elif cardinal_direction == Vector2.UP:
+		return "up"
+	else:
+		return "side"
