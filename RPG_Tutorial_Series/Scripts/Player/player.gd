e1f7370258548b78fd00dac54050983b2c0f2344
FelipeFlister <153518854+FelipeFlister@users.noreply.github.com>
2025-07-29 16:02:11 -0300

    Adição do tilemap layer e estradas

--- RPG_Tutorial_Series/Scripts/Player/player.gd
+++ RPG_Tutorial_Series/Scripts/Player/player.gd
@@ -11,7 +11,7 @@ func _ready() -> void:
 	state_machine.initialize(self)
 
 func  _process(_delta: float) -> void:
-	direction = Input.get_vector("left", "right", "up", "down")
+	direction = Input.get_vector("left", "right", "up", "down").normalized()
 	
 func _physics_process(_delta: float) -> void:
 	move_and_slide()
